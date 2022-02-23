1758d17b28ff63a269110b4419b00796a62a20c5
HappySonia <soniahuyan@gmail.com>
2022-02-23 20:48:01 +1000

    api added

--- controllers/weather_controller.rb
+++ controllers/weather_controller.rb
@@ -4,32 +4,34 @@ get '/weather/details' do
 end
 
 get '/weather/result' do
-    city = params['city']
-    duration = params['duration']
+    city = params['city']    
 
-    # url = "https://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=3e4e9811f1372c72678cc60e76137f0d&units=metric"
+    url_geo = "https://api.openweathermap.org/geo/1.0/direct?q=#{city}&limit=5&appid=3e4e9811f1372c72678cc60e76137f0d"
 
-    # url = "https://api.openweathermap.org/data/2.5/forecast/daily?q=#{city}&appid=3e4e9811f1372c72678cc60e76137f0d&units=metric"
+    result_geo = HTTParty.get(url_geo).parsed_response
 
+    lat = result_geo[0]['lat']
+    lon = result_geo[0]['lon']
+
+    url_weather = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{lon}&exclude=current,minutely,hourly,alerts&appid=3e4e9811f1372c72678cc60e76137f0d&units=metric"
+
+    result =  HTTParty.get(url_weather).parsed_response
     
+    erb :'weather/result', locals: {
+    city:result['timezone'],            
+    temp:result['daily'][0]['temp']['day'],
+    humidity:result['daily'][0]['humidity'],
+    wind_speed:result['daily'][0]['wind_speed'],
+    description:result['daily'][0]['weather'][0]['description'],
 
-    url_GEO = "https://api.openweathermap.org/geo/1.0/direct?q=#{city}&limit=5&appid=3e4e9811f1372c72678cc60e76137f0d"
+    temp2:result['daily'][1]['temp']['day'],    
+    humidity2:result['daily'][1]['humidity'],
+    wind_speed2:result['daily'][1]['wind_speed'],
+    description2:result['daily'][1]['weather'][0]['description'],
 
-    result = HTTParty.get(url_GEO).parsed_response
-
-    lat = result[0]['lat']
-    lon = result[0]['lon']
-
-    weather_url="https://api.openweathermap.org/data/2.5/forecast/daily?lat=#{lat}&lon=#{lon}&cnt=#{duration}appid=3e4e9811f1372c72678cc60e76137f0d"
-
-
-    api_result =  HTTParty.get(weather_url).parsed_response
-    
-    erb :'weather/result', locals: {            
-        description: api_result['weather'][0]["description"],
-        temp: api_result['main']['temp'],
-        humidity: api_result['main']['humidity'],
-        visibility: api_result['visibility'],
-        city:city
+    temp3:result['daily'][2]['temp']['day'],    
+    humidity3:result['daily'][2]['humidity'],
+    wind_speed3:result['daily'][2]['wind_speed'],
+    description3:result['daily'][2]['weather'][0]['description']
     }
 end
